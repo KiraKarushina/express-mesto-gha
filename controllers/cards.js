d0a82230acb8cd9ca95343a9ba62c7dda1d6aaf1
sergey.maximenko <sergey.maximenko@kaspersky.com>
2023-03-09 18:24:38 +0100

    handle errors part 5

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,56 +2,74 @@ const Card = require('../models/card');
 const {getErrorStatusCode} = require("../utils/helper");
 
 module.exports.createCard = (req, res) => {
-  const { name, link } = req.body;
+  const {name, link} = req.body;
   const owner = req.user._id;
 
-  Card.create({ name, link, owner })
-    .then((card) => res.status(200).send({ data: card }))
+  Card.create({name, link, owner})
+    .then((card) => res.status(200).send({data: card}))
     .catch((err) => {
       const codeStatus = getErrorStatusCode(err);
-      res.status(codeStatus).send({ message: err.message });
+      res.status(codeStatus).send({message: err.message});
     })
 };
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.status(200).send({ data: cards }))
+    .then((cards) => res.status(200).send({data: cards}))
     .catch((err) => {
       const codeStatus = getErrorStatusCode(err);
-      res.status(codeStatus).send({ message: err.message });
+      res.status(codeStatus).send({message: err.message});
     })
 };
 
 module.exports.deleteCard = (req, res) => {
   const id = req.params.cardId;
   Card.findByIdAndDelete(id)
-    .then((card) => res.status(200).send({ data: card }))
+    .then((card) => {
+      if (card) {
+        res.status(200).send({data: card})
+      } else {
+        res.status(404).send({message: 'Card not found'});
+      }
+    })
     .catch((err) => {
       const codeStatus = getErrorStatusCode(err);
-      res.status(codeStatus).send({ message: err.message });
+      res.status(codeStatus).send({message: err.message});
     })
 };
 
 module.exports.setLike = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-    { new: true },
-  ).then((card) => res.status(200).send({ data: card }))
+    {$addToSet: {likes: req.user._id}}, // добавить _id в массив, если его там нет
+    {new: true},
+  ).then((card) => {
+    if (card) {
+      res.status(200).send({data: card})
+    } else {
+      res.status(404).send({message: 'Card not found'});
+    }
+  })
     .catch((err) => {
       const codeStatus = getErrorStatusCode(err);
-      res.status(codeStatus).send({ message: err.message });
+      res.status(codeStatus).send({message: err.message});
     })
 };
 
 module.exports.deleteLike = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $pull: { likes: req.user._id } }, // убрать _id из массива
-    { new: true },
-  ).then((card) => res.status(200).send({ data: card }))
+    {$pull: {likes: req.user._id}}, // убрать _id из массива
+    {new: true},
+  ).then((card) => {
+    if (card) {
+      res.status(200).send({data: card})
+    } else {
+      res.status(404).send({message: 'Card not found'});
+    }
+  })
     .catch((err) => {
       const codeStatus = getErrorStatusCode(err);
-      res.status(codeStatus).send({ message: err.message });
+      res.status(codeStatus).send({message: err.message});
     })
 };
