f2385d6bfd3dfdd55cb4aee2b64f51a992a0410e
sergey.maximenko <sergey.maximenko@kaspersky.com>
2023-03-09 18:27:22 +0100

    handle errors part 5

--- utils/helper.js
+++ utils/helper.js
@@ -2,10 +2,10 @@ module.exports.getErrorStatusCode = (err) => {
 
   console.log('1111', err, err.name);
   if (err.name === 'ValidationError') {
-    return 400
+    return 400;
   }
   if (err.name === 'CastError') {
-    return 404
+    return 400;
   }
   return 500;
 }
